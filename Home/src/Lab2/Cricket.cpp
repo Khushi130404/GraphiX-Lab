d3aeab3ab5115b34932be82adcfdbd86a41e4cb5
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-04 20:32:34 +0530

    Inner Ovel

--- Home/src/Lab2/Cricket.cpp
+++ Home/src/Lab2/Cricket.cpp
@@ -6,7 +6,10 @@ int main()
     initgraph(&gd, &gm, "");
     setfillstyle(SOLID_FILL, GREEN);
     fillellipse(320, 240, 200, 200);
-    fillellipse(320, 240, 170, 170);
+    fillellipse(320, 240, 180, 180);
+    fillellipse(320, 240, 120, 100);
+    setfillstyle(SOLID_FILL, YELLOW);
+    bar(280, 220, 360, 260);
     getch();
     closegraph();
     return 0;
