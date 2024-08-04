20956fd16f329df011559580cfd9839d47d5743b
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-04 20:48:40 +0530

    Beige Not Working

--- Home/src/Lab2/Cricket.cpp
+++ Home/src/Lab2/Cricket.cpp
@@ -4,14 +4,12 @@ int main()
 {
     int gd = DETECT, gm;
     initgraph(&gd, &gm, "");
-    int beige = 20;
-    setrgbpalette(beige, 245, 245, 220);
     setfillstyle(SOLID_FILL, GREEN);
     fillellipse(320, 240, 200, 200);
     fillellipse(320, 240, 180, 180);
     fillellipse(320, 240, 120, 100);
-    setfillstyle(SOLID_FILL, beige);
-    bar(260, 220, 380, 260);
+    setfillstyle(SOLID_FILL, YELLOW);
+    bar(240, 210, 400, 270);
     getch();
     closegraph();
     return 0;
