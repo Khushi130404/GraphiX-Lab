8de2067b8e4d8ae9cd09b4d09c55c818ada80fdc
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-04 21:06:50 +0530

    Cricket.cpp

--- Home/src/Lab2/Cricket.cpp
+++ Home/src/Lab2/Cricket.cpp
@@ -10,6 +10,9 @@ int main()
     fillellipse(320, 240, 120, 100);
     setfillstyle(SOLID_FILL, YELLOW);
     bar(240, 210, 400, 270);
+    setlinestyle(0, 0, 1);
+    line(260, 155, 260, 325);
+    line(380, 155, 380, 325);
     getch();
     closegraph();
     return 0;
