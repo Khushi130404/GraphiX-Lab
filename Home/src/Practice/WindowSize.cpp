345554ce553498b600d791e6ecdc9e03b727eba4
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-04 21:19:37 +0530

    Window Size

--- Home/src/Practice/WindowSize.cpp
+++ Home/src/Practice/WindowSize.cpp
@@ -7,14 +7,14 @@ int main()
     int gd = DETECT, gm;
     initgraph(&gd, &gm, "");
 
+    // Graphics Window Width : 640
+    // Graphics Window Height : 480
+
     int width = getmaxx();
     int height = getmaxy();
 
     std::cout << "Graphics Window Width: " << width << std::endl;
     std::cout << "Graphics Window Height: " << height << std::endl;
-
-    // Graphics Window Width : 640
-    // Graphics Window Height : 480
 
     getch();
     closegraph();
