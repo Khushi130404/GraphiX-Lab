a1958cf0ac63245690638d1511c44e3807676c9f
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-09-13 23:17:32 +0530

    Liang_Barsky Final

--- Home/src/Lab5/Liang_Barsky.cpp
+++ Home/src/Lab5/Liang_Barsky.cpp
@@ -21,48 +21,49 @@ int main()
     line(x1, y1, x2, y2);
     rectangle(Xmin, Ymin, Xmax, Ymax);
     setcolor(RED);
-    int p[4];
+    float p[4];
     p[0] = x1 - x2;
     p[1] = x2 - x1;
     p[2] = y1 - y2;
     p[3] = y2 - y1;
-    int q[4];
+    float q[4];
     q[0] = x1 - Xmin;
     q[1] = Xmax - x1;
     q[2] = y1 - Ymin;
     q[3] = Ymax - y1;
     float t1 = 0, t2 = 1;
-    boolean flag = true;
+    int flag = 1;
     for (int i = 0; i < 4; i++)
     {
         if (p[i] == 0)
         {
-            printf("Line is parallel");
+            printf("\nLine is parallel");
             for (int i = 0; i < 4; i++)
             {
                 if (q[i] < 0)
                 {
-                    flag = false;
-                    printf("Line is Completely Outside");
+                    flag = 0;
+                    printf("\nLine is Completely Outside");
                     break;
                 }
             }
             break;
         }
-    }
-    if (flag)
-    {
-        for (int i = 0; i < 4; i++)
+        else
         {
+            float t = q[i] / p[i];
             if (p[i] < 0)
             {
-                t1 = max(0, q[i] / p[i]);
+                t1 = max(t1, t);
             }
             if (p[i] > 0)
             {
-                t2 = min(1, q[i] / p[i]);
+                t2 = min(t2, t);
             }
         }
+    }
+    if (flag)
+    {
         if (t1 < t2)
         {
             int x = x1, y = y1, xx = x2, yy = y2;
@@ -77,10 +78,12 @@ int main()
                 yy = y1 + t2 * (y2 - y1);
             }
             line(x, y, xx, yy);
+            line(x + 1, y + 1, xx + 1, yy + 1);
+            line(x - 1, y - 1, xx - 1, yy - 1);
         }
         else if (t1 > t2)
         {
-            printf("Line is Outside the window");
+            printf("\nLine is Outside the window");
         }
     }
     getch();
